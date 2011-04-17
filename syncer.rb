32b0f0c86a29901f06be9150fe7286ca4bd58328
BKrsta <bkrsta@gmail.com>
2011-04-17 20:11:50 +0200

    fix syncer - radi na 1.8 i na 1.9

--- syncer.rb
+++ syncer.rb
@@ -1,6 +1,8 @@
 %w[rubygems active_support/core_ext/numeric/time.rb open-uri ./gcal-lib ./ical2db ./db-lib].each{|x| require x}
 if RUBY_VERSION =~ /^1.8/
   require "system_timer"
+else
+  require "timeout"
 end
 
 def sync_cals(timeout=600)
@@ -16,15 +18,21 @@ def sync_cals(timeout=600)
 
   b = SeqConn.new *(File.readlines("db.conf").first[/(.+)\n?/,1].split(":")[0..3])
 
+  blo = Proc.new {
+    urls.each{|d|
+      x = File.readlines("db.conf").first[/(.+)\n?/,1].split(":")[0..3] + [b.raz_id(*d[0].split("_"))]
+      cl = Ical2db.new d[1], *x # url, host, user, pass, db, raz_id
+      puts "Sinkroniziram razred '#{d[0]}'\nCAL_URL: '#{d[1]}'"
+      cl.sync
+    }
+  }
+
   b.open
   begin
-    SystemTimer.timeout_after(timeout.seconds) do
-      urls.each{|d|
-        x = File.readlines("db.conf").first[/(.+)\n?/,1].split(":")[0..3] + [b.raz_id(*d[0].split("_"))]
-        cl = Ical2db.new d[1], *x # url, host, user, pass, db, raz_id
-        puts "Sinkroniziram razred '#{d[0]}'\nCAL_URL: '#{d[1]}'"
-        cl.sync
-      }
+    if RUBY_VERSION =~ /^1.8/
+      SystemTimer.timeout_after(timeout.seconds){ blo.call }
+    else
+      timeout(timeout){ blo.call }
     end
   rescue Timeout::Error
     puts "sync_cals() timeout! (#{timeout} sec.)"
